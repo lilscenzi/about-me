a001ae85fb5cca60bfefc2d3fdbd3ff8d7b621b2
lilscenzi <103005927+lilscenzi@users.noreply.github.com>
2022-04-04 22:08:54 -0400

    added text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hi i am very cool and poggers 
+      hi i am very cool and poggers
+      i love chia so much because she was nice
+      aqua was not good :()
     </div>
   );
 }
