42622240a46474e4e616036b182a67dc8afae3e9
lilscenzi <103005927+lilscenzi@users.noreply.github.com>
2022-04-05 19:02:27 -0400

    image, color, button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function whenIClick(){
+    alert("YOU HAVE BEEN HACKED BRING $7500 TO 25.004044, -71.002575 IMMEDIATLY");
+  }
   return (
-    <div className="App">
+    <div id="everything">
       hi i am very cool and poggers
       <div> <h1>Im extremely pog</h1></div>
        you couldn't find anyone better than me even if you tried 
-     <div> <h1> I'm also secretly 33 irl </h1><div>
-    </div>
+     <div> <h3> I'm also secretly 33 irl </h3></div>
+     <button
+        onClick={whenIClick}
+        id="thisisbutton"
+     > click me or else i will consume leos kneecaps</button>
+     <div><img id="rich" src="https://thumbs.dreamstime.com/b/very-rich-man-smiles-shows-off-bundles-hundred-dollar-bills-focus-dollars-not-face-92362139.jpg"
+
+     ></img></div>
+     <div id="poor">I shall steal all your money and then you will be poor hahhah </div>
+    <h2> AHUHUYQU(HUISDHUIAHUIHUIDHUIAUHI</h2>
+     </div>
   );
 }
 
