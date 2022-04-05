91c67d0226eba2e962a7fcc515f8f2130ab085b6
lilscenzi <103005927+lilscenzi@users.noreply.github.com>
2022-04-05 18:22:25 -0400

    updated text

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       hi i am very cool and poggers
       <div> <h1>Im extremely pog</h1></div>
-      <div> you couldn't find anyone better than me even if you tried <div>
-      <h1> I'm also secretly 33 irl </h1>
+       you couldn't find anyone better than me even if you tried 
+     <div> <h1> I'm also secretly 33 irl </h1><div>
     </div>
   );
 }
